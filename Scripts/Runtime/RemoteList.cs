d4be11fd89d8aac6d55ebade888f6132087ce79e
Chris Walka <walka.christopher@gmail.com>
2023-05-11 18:25:39 -0500

    Added namespace

--- Scripts/Runtime/RemoteList.cs
+++ Scripts/Runtime/RemoteList.cs
@@ -7,74 +7,77 @@ using Newtonsoft.Json;
 
 namespace CodySource
 {
-    public class RemoteList : MonoBehaviour
+    namespace RemoteList
     {
-
-        #region PROPERTIES
-
-        private static RemoteList _instance;
-        public static RemoteList instance
+        public class RemoteList : MonoBehaviour
         {
-            get
+
+            #region PROPERTIES
+
+            private static RemoteList _instance;
+            public static RemoteList instance
             {
-                if (_instance == null)
+                get
                 {
-                    RemoteList[] _all = FindObjectsOfType<RemoteList>();
-                    _instance = _all.Length>0?_all[0]:null;
-                    if (_instance != null) return _instance;
-                    GameObject obj = new GameObject();
-                    obj.hideFlags = HideFlags.HideAndDontSave;
-                    _instance = obj.AddComponent<RemoteList>();
+                    if (_instance == null)
+                    {
+                        RemoteList[] _all = FindObjectsOfType<RemoteList>();
+                        _instance = _all.Length>0?_all[0]:null;
+                        if (_instance != null) return _instance;
+                        GameObject obj = new GameObject();
+                        obj.hideFlags = HideFlags.HideAndDontSave;
+                        _instance = obj.AddComponent<RemoteList>();
+                    }
+                    return _instance;
                 }
-                return _instance;
             }
-        }
 
-        public string defaultURL = "";
-        public UnityEvent<string> onRequestFailed = new UnityEvent<string>();
-        public UnityEvent<Response> onRequestComplete = new UnityEvent<Response>();
+            public string defaultURL = "";
+            public UnityEvent<string> onRequestFailed = new UnityEvent<string>();
+            public UnityEvent<Response> onRequestComplete = new UnityEvent<Response>();
 
-        #endregion
+            #endregion
 
-        #region PUBLIC METHODS
+            #region PUBLIC METHODS
 
-        /// <summary>
-        /// Loads the manifest of the streaming assets contents
-        /// </summary>
-        public void RequestList(string pURL = "", string pFilter = "*", string pKey = "") => StartCoroutine(_Request((pURL=="")?defaultURL:pURL, pFilter, pKey));
+            /// <summary>
+            /// Loads the manifest of the streaming assets contents
+            /// </summary>
+            public void RequestList(string pURL = "", string pFilter = "*", string pKey = "") => StartCoroutine(_Request((pURL=="")?defaultURL:pURL, pFilter, pKey));
 
-        #endregion
+            #endregion
 
-        #region INTERNAL METHODS
+            #region INTERNAL METHODS
 
-        /// <summary>
-        /// Perform the request
-        /// </summary>
-        internal IEnumerator _Request(string pURL, string pFilter = "*", string pKey = "")
-        {
-            WWWForm form = new WWWForm();
-            if (pKey != "") form.AddField("key", pKey);
-            form.AddField("f", $"{pFilter}");
-            using (UnityWebRequest www = UnityWebRequest.Post($"{pURL}", form))
+            /// <summary>
+            /// Perform the request
+            /// </summary>
+            internal IEnumerator _Request(string pURL, string pFilter = "*", string pKey = "")
             {
-                yield return www.SendWebRequest();
-                if (www.result != UnityWebRequest.Result.Success) onRequestFailed?.Invoke(www.error);
-                else onRequestComplete?.Invoke(JsonConvert.DeserializeObject<Response>(www.downloadHandler.text));
+                WWWForm form = new WWWForm();
+                if (pKey != "") form.AddField("key", pKey);
+                form.AddField("f", $"{pFilter}");
+                using (UnityWebRequest www = UnityWebRequest.Post($"{pURL}", form))
+                {
+                    yield return www.SendWebRequest();
+                    if (www.result != UnityWebRequest.Result.Success) onRequestFailed?.Invoke(www.error);
+                    else onRequestComplete?.Invoke(JsonConvert.DeserializeObject<Response>(www.downloadHandler.text));
+                }
             }
+
+            #endregion
+
+            #region PUBLIC STRUCTS
+
+            [System.Serializable]
+            public struct Response
+            {
+                public string error;
+                public string[] contents;
+            }
+
+            #endregion
+
         }
-
-        #endregion
-
-        #region PUBLIC STRUCTS
-
-        [System.Serializable]
-        public struct Response
-        {
-            public string error;
-            public string[] contents;
-        }
-
-        #endregion
-
     }
 }
