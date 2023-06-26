6e3070f4a1a01878d8ff5be118e40a763731b17b
Chris Walka <walka.christopher@gmail.com>
2023-06-26 13:22:07 -0500

    Update RemoteList.php

--- Scripts/Runtime/RemoteList.php
+++ Scripts/Runtime/RemoteList.php
@@ -1,6 +1,6 @@
 <?php 
 const FILTERS = array();
-if (!isset($_POST['key']||!array_key_exists($_POST['key'],FILTERS)) o(null,'Invalid key supplied.');
+if (!isset($_POST['key'])||!array_key_exists($_POST['key'],FILTERS)) o(null,'Invalid key supplied.');
 if (!isset($_POST['f'])||$_POST['f']=='') o(null,'No filter supplied.');
 if (preg_match('/[^a-zA-Z0-9 _.\\/*-]/', $_POST['f'])||!in_array($_POST['f'],FILTERS[$_POST['key']])) o(null,'Invalid filter supplied.');
 o(array_map(fn($v)=>basename($v),glob($_POST['f'])),null);
