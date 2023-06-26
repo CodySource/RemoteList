f38e019fa72a76d47cac19f554886b8e85c21cae
Chris Walka <walka.christopher@gmail.com>
2023-06-26 12:43:09 -0500

    Update RemoteList.php

--- Scripts/Runtime/RemoteList.php
+++ Scripts/Runtime/RemoteList.php
@@ -1,8 +1,8 @@
 <?php 
-$key=file_get_contents('key.txt');
-if ($key&&(!isset($_POST['key'])||$_POST['key']!=$key)) o(null,'Invalid key supplied.');
+const FILTERS = array();
+if (!isset($_POST['key']||!array_key_exists($_POST['key'],FILTERS)) o(null,'Invalid key supplied.');
 if (!isset($_POST['f'])||$_POST['f']=='') o(null,'No filter supplied.');
-if (preg_match('/[^a-zA-Z0-9 _.\\/*-]/', $_POST['f'])) o(null,'Invalid filter supplied.');
+if (preg_match('/[^a-zA-Z0-9 _.\\/*-]/', $_POST['f'])||!in_array($_POST['f'],FILTERS[$_POST['key']])) o(null,'Invalid filter supplied.');
 o(array_map(fn($v)=>basename($v),glob($_POST['f'])),null);
 function o($c,$e){die(json_encode((object)array('contents'=>$c,'error'=>$e)));}
 ?>
